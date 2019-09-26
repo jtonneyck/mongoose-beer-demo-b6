1a291d69a9ab856cf9b2e457697929fad823ca21
Piepongwong <j.tonneyck@gmail.com>
2019-09-26 10:15:08 +0200

    rewriting to get request queries

--- routes/beers.js
+++ routes/beers.js
@@ -12,4 +12,26 @@ app.get("/beers", (req, res)=> {
         })
 })
 
+app.get("/beer/:beerId", (req, res)=> {
+    debugger
+    Beer.findById(req.params.beerId)
+        .then((beer)=> {
+            res.render("beer-detail", {beer:beer})
+        })
+        .catch((err)=> {
+            res.send(err)
+        })
+})
+
+app.get("/beer/delete/:beerId", (req, res)=> {
+    debugger
+    Beer.findOneAndRemove(req.params.beerId)
+        .then((beer)=> {
+            res.redirect("/beers")
+        })
+        .catch((err)=> {
+            res.send(err)
+        })
+})
+
 module.exports = app
